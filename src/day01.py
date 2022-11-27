4224903015f1773088dce4b77ccc8508f1fa37eb
Stuart McHattie <stuart.mchattie@gmail.com>
2022-11-27 21:44:51 +0000

    Set up a daily template for day01

---
+++ src/day01.py
@@ -0,0 +1,20 @@
+from lib.file_access import read_input_lines
+
+RAW_INPUT = read_input_lines(__file__)
+
+
+def part1():
+    print("Part 1")
+    print(f"  Solution to part 1")
+
+
+def part2():
+    print("Part 2")
+    print(f"  Solution to part 2")
+
+
+print()
+part1()
+print()
+part2()
+print()
---
+++ src/lib/file_access.py
@@ -0,0 +1,13 @@
+from os import path
+
+
+def default_input_file(script_file_path):
+    script_root = path.splitext(script_file_path)
+    file_root = path.basename(script_root[0])
+
+    return f"../input/{file_root}.txt"
+
+
+def read_input_lines(script_file_path):
+    with open(default_input_file(script_file_path), "r") as file:
+        return [line.strip() for line in file.readlines()]

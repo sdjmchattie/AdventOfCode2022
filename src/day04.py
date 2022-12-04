4532db430fc924734d20c4f52d98d388454171de
Stuart McHattie <stuart.mchattie@gmail.com>
2022-12-04 12:07:55 +0000

    Tidy up input parsing

--- src/day04.py
+++ src/day04.py
@@ -11,9 +11,10 @@ class Day04:
 
     def process_input(self):
         def split_values(line):
-            vals = re.split(SPLIT_REGEX, line)
-            range_1 = set(range(int(vals[0]), int(vals[1]) + 1))
-            range_2 = set(range(int(vals[2]), int(vals[3]) + 1))
+            split_vals = re.split(SPLIT_REGEX, line)
+            vals = list(map(int, split_vals))
+            range_1 = set(range(vals[0], vals[1] + 1))
+            range_2 = set(range(vals[2], vals[3] + 1))
 
             return (range_1, range_2)
 

fa992796ef3ab783a1ca834930efa75378ea1dde
Stuart McHattie <stuart.mchattie@gmail.com>
2022-12-04 11:57:44 +0000

    Add solution for day 4 part 2

--- src/day04.py
+++ src/day04.py
@@ -11,30 +11,27 @@ class Day04:
 
     def process_input(self):
         def split_values(line):
-            split_vals = re.split(SPLIT_REGEX, line)
-            return list(map(lambda v: int(v), split_vals))
+            vals = re.split(SPLIT_REGEX, line)
+            range_1 = set(range(int(vals[0]), int(vals[1]) + 1))
+            range_2 = set(range(int(vals[2]), int(vals[3]) + 1))
+
+            return (range_1, range_2)
 
         self.ranges = list(map(split_values, self.raw_input))
 
     def part1(self):
-        def ranges_fully_overlap(ranges):
-            return (
-                ranges[0] == ranges[2]
-                or ranges[1] == ranges[3]
-                or (ranges[0] > ranges[2] and ranges[1] <= ranges[3])
-                or (ranges[0] < ranges[2] and ranges[1] >= ranges[3])
-            )
-
-        subset_count = sum(map(ranges_fully_overlap, self.ranges))
+        count = sum(map(lambda r: len(r[0] - r[1]) == 0 or len(r[1] - r[0]) == 0, self.ranges))
 
         print()
         print("Part 1")
-        print(f"  Solution to part 1: {subset_count}")
+        print(f"  Solution to part 1: {count}")
 
     def part2(self):
+        count = sum(map(lambda r: len(r[0]) != len(r[0] - r[1]), self.ranges))
+
         print()
         print("Part 2")
-        print(f"  Solution to part 2: ")
+        print(f"  Solution to part 2: {count}")
 
 
 puzzle = Day04()

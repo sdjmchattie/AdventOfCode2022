e563347fdcc1edb43332ee7c40cc20de5c567d02
Stuart McHattie <sm49@sanger.ac.uk>
2022-12-13 22:16:20 +0000

    Add solution for day 13 part 2

--- src/day13.py
+++ src/day13.py
@@ -1,3 +1,5 @@
+from functools import cmp_to_key
+
 from lib.file_access import read_input_lines
 
 
@@ -5,20 +7,27 @@ class Day13:
     def __init__(self):
         self._raw_input = read_input_lines(__file__)
 
-    def process_input(self):
+    def part1(self):
         pairs = ((eval(self._raw_input[i]), eval(self._raw_input[i+1])) for i in range(0, len(self._raw_input), 3))
         indices = (index + 1 if Day13._compare(pair[0], pair[1]) == -1 else 0 for index, pair in enumerate(pairs))
         self._pairs_in_order = sum(indices)
 
-    def part1(self):
         print()
         print("Part 1")
         print(f"  Solution to part 1: {self._pairs_in_order}")
 
     def part2(self):
+        signals = [eval(x) for x in filter(lambda line: line != "", self._raw_input)]
+        signals.append([[2]])
+        signals.append([[6]])
+        signals.sort(key=cmp_to_key(Day13._compare))
+
+        divider_2 = signals.index([[2]]) + 1
+        divider_6 = signals.index([[6]]) + 1
+
         print()
         print("Part 2")
-        print(f"  Solution to part 2: ")
+        print(f"  Solution to part 2: {divider_2 * divider_6}")
 
     @staticmethod
     def _compare(left, right):
@@ -47,6 +56,5 @@ class Day13:
 
 
 puzzle = Day13()
-puzzle.process_input()
 puzzle.part1()
 puzzle.part2()

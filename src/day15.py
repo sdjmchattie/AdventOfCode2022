60cfa8bda1ceda6566a37d5a5b4092de52746638
Stuart McHattie <stuart.mchattie@tessella.com>
2022-12-15 22:52:47 +0000

    Add day 15 part 2 solution

--- src/day15.py
+++ src/day15.py
@@ -51,33 +51,44 @@ class Day15:
         vals = [list(map(int, REGEX_INPUT_LINE.match(line).groups())) for line in self._raw_input]
         self._sensors = [Sensor(Point(sx, sy), Point(bx, by)) for sx, sy, bx, by in vals]
 
-    def part1(self):
-        row_ranges = filter(
-            lambda v: v is not None, (sensor.coverage_range_on_row(2000000) for sensor in self._sensors)
-        )
+    def coalesced_ranges_of_coverage_on_row(self, row):
+        row_ranges = filter(lambda v: v is not None, (sensor.coverage_range_on_row(row) for sensor in self._sensors))
 
-        coallesced_ranges = []
+        coalesced_ranges = []
         for new_range in row_ranges:
             overlapped_ranges = [new_range]
-            for range in coallesced_ranges[:]:
+            for range in coalesced_ranges[:]:
                 if range.overlaps_with(new_range):
                     overlapped_ranges.append(range)
-                    coallesced_ranges.remove(range)
+                    coalesced_ranges.remove(range)
 
             min_x = min(overlapped_ranges, key=lambda r: r.min).min
             max_x = max(overlapped_ranges, key=lambda r: r.max).max
-            coallesced_ranges.append(Range(min_x, max_x))
+            coalesced_ranges.append(Range(min_x, max_x))
 
-        beacons_on_row = set((sensor.beacon for sensor in self._sensors if sensor.beacon.y == 2000000))
+        return coalesced_ranges
 
+    def part1(self):
         print()
         print("Part 1")
+
+        coallesced_ranges = self.coalesced_ranges_of_coverage_on_row(2000000)
+        beacons_on_row = set((sensor.beacon for sensor in self._sensors if sensor.beacon.y == 2000000))
+
         print(f"  Solution to part 1: {sum(map(lambda r: r.length(), coallesced_ranges)) - len(beacons_on_row)}")
 
     def part2(self):
         print()
         print("Part 2")
-        print(f"  Solution to part 2: ")
+
+        beacon_point = None
+        for row in range(0, 4000001):
+            coalesced_ranges = self.coalesced_ranges_of_coverage_on_row(row)
+            if len(coalesced_ranges) == 2:
+                x = min(coalesced_ranges[0].max + 1, coalesced_ranges[1].max + 1)
+                beacon_point = Point(x, row)
+
+        print(f"  Solution to part 2: {beacon_point.x * 4000000 + beacon_point.y}")
 
 
 puzzle = Day15()

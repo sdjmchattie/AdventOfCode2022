c0f132b4040767b667ea6080faffd6c95d63ab9c
Stuart McHattie <stuart.mchattie@tessella.com>
2022-12-18 17:10:46 +0000

    Add solution for day 18 part 2

--- src/day18.py
+++ src/day18.py
@@ -13,6 +13,10 @@ class Model3D:
     def exposed_surface_area(self):
         return sum(map(lambda voxel: self._exposed_faces_of_voxel(voxel), self._voxels))
 
+    @cached_property
+    def exterior_surface_area(self):
+        return self._surface_area_by_flood_fill()
+
     def _exposed_faces_of_voxel(self, voxel):
         exposed_faces = 6
         for axis, value in enumerate(voxel):
@@ -23,6 +27,37 @@ class Model3D:
                     exposed_faces -= 1
 
         return exposed_faces
+
+    def _surface_area_by_flood_fill(self):
+        surface_area = 0
+        to_visit = {(-1, -1, -1)}
+        visited = set()
+
+        while len(to_visit) > 0:
+            visiting = to_visit.pop()
+            visited.add(visiting)
+
+            # Get all 6 adjacent voxels
+            adjacent = []
+            for axis, value in enumerate(visiting):
+                for delta in (-1, 1):
+                    new_adjacent = list(visiting)
+                    new_adjacent[axis] = value + delta
+                    adjacent.append(tuple(new_adjacent))
+
+            # Remove out of bounds voxels
+            adjacent = set(
+                filter(
+                    lambda a: a[0] >= -1 and a[1] >= -1 and a[2] >= -1 and a[0] <= 20 and a[1] <= 20 and a[2] <= 20,
+                    adjacent,
+                )
+            )
+
+            model_adjacent = self._voxels.intersection(adjacent)
+            surface_area += len(model_adjacent)
+            to_visit.update(adjacent.difference(visited).difference(model_adjacent))
+
+        return surface_area
 
 
 class Day18:
@@ -45,7 +80,10 @@ class Day18:
     def part2(self):
         print()
         print("Part 2")
-        print(f"  Solution to part 2: ")
+
+        model = Model3D(self.voxels)
+
+        print(f"  Solution to part 2: {model.exterior_surface_area}")
 
 
 puzzle = Day18()

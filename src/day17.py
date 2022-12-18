63ef749a7941e38cbc86439953c14397991e438f
Stuart McHattie <stuart.mchattie@tessella.com>
2022-12-18 13:38:27 +0000

    Add day 17 part 2 solution

--- src/day17.py
+++ src/day17.py
@@ -37,6 +37,9 @@ class RockFormation:
     def __init__(self, formation):
         self.formation = formation
 
+    def __str__(self):
+        return "\n".join(self.formation)
+
     def rock_positions(self, offset):
         for y, row in enumerate(self.formation):
             for x, symbol in enumerate(row):
@@ -122,13 +125,78 @@ class Day17:
         print(f"  Solution to part 1: {tetris.tower_height}")
 
     def part2(self):
+        """
+        We can use the following code to spot the pattern that repeats every time the moves loop:
+
+        for i in range(3):
+            for _ in range(len(self.raw_input[0])):
+                tetris.move_and_drop()
+            print()
+            print(f"After {(i + 1) * len(self.raw_input[0])} moves:")
+            print(str(tetris)[:70])
+            print(f"Rocks fallen: {tetris.fallen}")
+            print(f"Currently falling rock:\n{tetris.falling_rock}")
+            print(f"Falling rock position: {tetris.falling_position}")
+            print(f"Tower height: {tetris.tower_height}")
+
+        In this case, the top of the board looks like this at the time that we repeat the move list:
+
+        |..#....|
+        |..#....|
+        |###....|
+        |.#.....|
+        |###....|
+        |.#####.|
+        |#....##|
+
+        The currently falling rock the vertical beam and it has a position of (x = 5, y = tower_height - 2).
+        The number of rocks that have fallen after a loop are 1718 + (1725 * (move_loop_count - 1)).
+        So the number of loops of the moves needed to reach 1000000000000 rocks stopped will be:
+
+        (1000000000000 - 1718) / 1725 + 1 = 579710144.9315943
+
+        The number of rocks fallen at the end of loop 579710144 are 999999998393
+
+        The height of the tower at the end of a loop can be determined using 2689 + (2728 * (move_loop_count - 1))
+        The height of the tower at the end of loop 579710144 will be 1581449272793.
+
+        So we can input these parameters as the starting point for a Tetris object and run until we get to 1000000000000.
+        Because the history of the stack is unimportant, we will use the image above to create a base of rocks in the tower.
+        """
         print()
         print("Part 2")
-        print(f"  Solution to part 2: ")
+
+        tetris = Tetris(self.moves())
+
+        tetris.rock = {
+            (0, 0),
+            (5, 0),
+            (6, 0),
+            (1, 1),
+            (2, 1),
+            (3, 1),
+            (4, 1),
+            (5, 1),
+            (0, 2),
+            (1, 2),
+            (2, 2),
+            (1, 3),
+            (0, 4),
+            (1, 4),
+            (2, 4),
+            (2, 5),
+            (2, 6),
+        }
+        tetris.fallen = 999999998393
+        tetris.falling_rock = RockFormation(FORMATIONS[3])
+        tetris.falling_position = Point(5, 5)
+
+        while tetris.fallen < 1000000000000:
+            tetris.move_and_drop()
+
+        print(f"  Solution to part 2: {1581449272793 - 7 + tetris.tower_height}")
 
 
 puzzle = Day17()
 puzzle.part1()
 puzzle.part2()
-
-# 3066 too low

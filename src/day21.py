83501461c5943e6cf10858948631a88c9d9382b0
Stuart McHattie <stuart.mchattie@tessella.com>
2023-01-10 20:57:46 +0000

    Add day 21 part 2 solution

--- src/day21.py
+++ src/day21.py
@@ -29,13 +29,22 @@ class Monkey:
             elif op_match[2] == "/":
                 return left_val / right_val
 
+    def check_equality_operation(self):
+        op_match = operation_regex.match(self._operation)
+        left_val = self.all_monkeys[op_match[1]].value
+        right_val = self.all_monkeys[op_match[3]].value
+        print(left_val)
+        print(right_val)
+        print()
+
+        return left_val == right_val
+
 
 class Day21:
     @cached_property
     def raw_input(self):
         return read_input_lines(__file__)
 
-    @cached_property
     def monkeys(self):
         matches = (line_regex.match(line) for line in self.raw_input)
         monkeys = {match[1]: Monkey(match[2]) for match in matches}
@@ -45,16 +54,25 @@ class Day21:
         return monkeys
 
     def part1(self):
-        root_monkey = self.monkeys["root"]
+        root_monkey = self.monkeys()["root"]
 
         print()
         print("Part 1")
         print(f"  Solution to part 1: {root_monkey.value}")
 
     def part2(self):
+        humn_val = 3876027196000
+        while True:
+            monkeys = self.monkeys()
+            monkeys["humn"]._operation = str(humn_val)
+            if monkeys["root"].check_equality_operation():
+                break
+
+            humn_val += 1
+
         print()
         print("Part 2")
-        print(f"  Solution to part 2: ")
+        print(f"  Solution to part 2: {humn_val}")
 
 
 puzzle = Day21()
